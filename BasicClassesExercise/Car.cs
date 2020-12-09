ace4ba6e387180c4e486223a38d963a86397b0f5
Amir Gaffar <AmirG7@yahoo.com>
2020-12-09 00:57:26 -0500

    Completed Classes Exercise

---
+++ BasicClassesExercise/Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace BasicClassesExercise
+{
+    public class Car
+    {
+
+
+        public Car()
+        {
+
+        }
+
+
+
+        public Car(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+
+
+        
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+    }
+}
